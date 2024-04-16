193d33e370b58e842d627a3a64c92f39daba3c10
BatuCem <63973576+BatuCem@users.noreply.github.com>
2024-04-16 23:54:50 +0300

    Update bldc_esc.v

--- src/bldc_esc.v
+++ src/bldc_esc.v
@@ -5,7 +5,6 @@
 // Create Date: 17.01.2024 11:44:23
 // Design Name: BLDC
 // Module Name: bldc_esc_1
-// Project Name: PID Controller for BLDC
 // Target Devices: 
 // Tool Versions: 
 // Description: 
@@ -21,27 +20,26 @@
 
 
 module bldc_esc_1 #(parameter DATA_WIDTH = 16,parameter debounce = 3)(
-  input clk,              		// clock input. when there is no clk signal motor does not run
+  input clk,
   input clk_div,
-  input reset,            		// when it is 1 motor does run active high
+  input reset,
   input [3:0] tunerreset_autotune,
-  input pwm_en,						//Pin to enable pwm output	active high
-  input encoder_a,  				// encoder A pin input
-  input encoder_b,  				// encoder B pin input
-  input [DATA_WIDTH-1:0] pwm_period,	//change period in clock cycles
-  input [DATA_WIDTH-1:0] period_reference,	// period_reference speed input, commented out since the ambiguity, will try with constant 900RPM period_reference speed
-  input [7:0] Kp_ext,//External proportional constant
-  input [7:0] Ki_ext,//External integral constant
-  input [6:0] Kd_ext,//External derivative constant
-  input override_internal_pid,		//select pin for external/internal pid constants
+  input pwm_en,
+  input encoder_a, 
+  input encoder_b,  			
+  input [DATA_WIDTH-1:0] pwm_period,	
+  input [DATA_WIDTH-1:0] period_reference,	
+  input [7:0] Kp_ext,
+  input [7:0] Ki_ext,
+  input [6:0] Kd_ext,
+  input override_internal_pid,		
   output [7:0] Kp_int_o,
-  //output [15:0] outputvar,  //TEST OUTPUT PINS
-  output reg motor_positive,   		// BLDC motor PWM signal
-  output reg motor_negative 	    // Motor direction control (1 bit)
+  output reg motor_positive,   	
+  output reg motor_negative
 );
     assign Kp_int_o = Kp_int;
     reg [DATA_WIDTH-1:0] period_reference_reg;
-	reg [DATA_WIDTH-1:0] period_speed ;// Counter connected to rise of encoder A that will infer speed by measuring period of clk cycles
+	reg [DATA_WIDTH-1:0] period_speed ;
 	wire tuning_done;
 	wire [7:0] Kp_int;
 	
@@ -57,13 +55,10 @@ module bldc_esc_1 #(parameter DATA_WIDTH = 16,parameter debounce = 3)(
     reg [1:0]encoder_state;
     reg [1:0]prev_encoder_state;
     reg [1:0]pwm_direction;
-  //PID control////////////////////////////////////////////////
-  // PID constants
     reg [DATA_WIDTH-1:0] speed_ctr;
     reg [7:0] Kp ;
     reg [7:0] Ki ;
     reg [6:0] Kd ;
-  // Variables for PID control
     reg signed[DATA_WIDTH-1:0] error;
     reg signed[(DATA_WIDTH)-1:0] integral;
     reg signed[DATA_WIDTH-1:0] derivative;
@@ -85,17 +80,17 @@ module bldc_esc_1 #(parameter DATA_WIDTH = 16,parameter debounce = 3)(
 	   .pid_select(tunerreset_autotune[2:0]),
 	   .period_speed(error),
 	   .tuning_done(tuning_done),
-	   .Kp(Kp_int),	//internal registers are read
+	   .Kp(Kp_int),	
 	   .Ki(Ki_int),
 	   .Kd(Kd_int)
     );
     
-    assign motor_pwm = (pwm_counter < pwm_duty_cycle) & pwm_en_reg; // Generate PWM signal and send it to the motor
+    assign motor_pwm = (pwm_counter < pwm_duty_cycle) & pwm_en_reg; 
     
     always @(posedge clk) 
     begin
  
-        if (reset) begin    //reset case
+        if (reset) begin 
             counter_rst <= 1'b1;
             encoder_a_set <= 2'b0;
             flag <= 1'b0;
@@ -125,26 +120,26 @@ module bldc_esc_1 #(parameter DATA_WIDTH = 16,parameter debounce = 3)(
         end else begin
             pwm_en_shift_reg <= {pwm_en_shift_reg[debounce-2:0],pwm_en};
             if (pwm_en_shift_reg == {debounce{1'b0}} || pwm_en_shift_reg == {debounce{1'b1}})begin
-                pwm_en_reg <= pwm_en_shift_reg[0];  //enable pwm
+                pwm_en_reg <= pwm_en_shift_reg[0];  
             end
             encoder_a_shift_reg <= {encoder_a_shift_reg[debounce-2:0],encoder_a};
             if (encoder_a_shift_reg == {debounce{1'b0}} || encoder_a_shift_reg == {debounce{1'b1}})begin
-                encoder_a_reg <= encoder_a_shift_reg[0];    //apply ancoder a
+                encoder_a_reg <= encoder_a_shift_reg[0]; 
             end 
             encoder_b_shift_reg <= {encoder_b_shift_reg[debounce-2:0],encoder_b};
             if (encoder_b_shift_reg == {debounce{1'b0}} || encoder_b_shift_reg == {debounce{1'b1}})begin
-                encoder_b_reg <= encoder_b_shift_reg[0];    //apply encoder b
+                encoder_b_reg <= encoder_b_shift_reg[0];  
             end
-            pid_output <= (Kp * error) + ((Ki * integral)>>3) + (Kd * derivative);	//compute pid response
-            if(pid_output<1) begin			//if pid output is negative, cap at 0
+            pid_output <= (Kp * error) + ((Ki * integral)>>3) + (Kd * derivative);	
+            if(pid_output<1) begin			
                 pwm_duty_cycle<=pwm_period;
-            end else if (pid_output>pwm_period) begin	//if pid output above period, give 100% pwm
+            end else if (pid_output>pwm_period) begin	
                 pwm_duty_cycle<=0;
             end else begin
-                pwm_duty_cycle<=pid_output;	//if in between values, give pid output as pwm
+                pwm_duty_cycle<=pid_output;	
             end
             derivative <= error - previous_error;
-            if (integral + error > 2048) begin //These are very high values for ASIC, need to push for lower register(?)
+            if (integral + error > 2048) begin 
                 integral <= 2047;
             end
             else if (integral + error < -2048) begin
@@ -156,7 +151,6 @@ module bldc_esc_1 #(parameter DATA_WIDTH = 16,parameter debounce = 3)(
             previous_error<=error;
             error <= period_reference_reg - period_speed;
                 
-            // Reset PWM counter at the end of the PWM period
             if (pwm_counter == pwm_period-1) begin
                 pwm_counter <= 16'd0;
             end else begin
@@ -166,24 +160,17 @@ module bldc_esc_1 #(parameter DATA_WIDTH = 16,parameter debounce = 3)(
             prev_encoder_state <= encoder_state;
             if(pwm_en_reg)begin
                 case ({encoder_state, prev_encoder_state})
-                    4'b0100, 4'b1101,4'b1011: begin// Rotate in one direction (e.g., forward)
+                    4'b0100, 4'b1101,4'b1011: begin
                         pwm_direction<=2'b10;
                     end
-                    4'b1000,4'b1110,4'b0111: begin// Rotate in the opposite direction (e.g., reverse)
+                    4'b1000,4'b1110,4'b0111: begin
                         pwm_direction<=2'b01;
                     end
-//                    4'b1100, 4'b0011, 4'b1001,4'b0110: begin// No change (no rotation)
-//                        pwm_direction<=2'b00;
-//                    end
                     default: begin
-                        pwm_direction<=pwm_direction;// Keep the previous direction in case of an unexpected state
+                        pwm_direction<=2'b00;
                     end
                 endcase
             end
-//            else begin
-//                encoder_state <= 2'b0;
-//                prev_encoder_state <= 2'b0;
-//            end
             
             if(!flag && (pwm_period!=0))begin
                 motor_positive <= (period_reference<32767) ? (1'b1) : (1'b0);
@@ -192,7 +179,7 @@ module bldc_esc_1 #(parameter DATA_WIDTH = 16,parameter debounce = 3)(
             end
             if(period_reference>=32767)
             begin
-                period_reference_reg<= (~period_reference)+1; //two's complement for negative references
+                period_reference_reg<= (~period_reference)+1;
             end else begin
                 period_reference_reg<= period_reference;
             end
@@ -202,7 +189,7 @@ module bldc_esc_1 #(parameter DATA_WIDTH = 16,parameter debounce = 3)(
             end else if (flag)begin
                 case(pwm_direction)
                     2'b00: begin
-                        if(period_reference>32767) begin //check negative references by 2's complement
+                        if(period_reference>32767) begin
                             motor_positive<=1'b0;
                             motor_negative<=motor_pwm;
                         end else begin
@@ -254,16 +241,4 @@ module bldc_esc_1 #(parameter DATA_WIDTH = 16,parameter debounce = 3)(
             end
     end
 end
- 
-  //assign outputvar = error; //TEST assignment
-  //(* mark_debug = "true" *) wire [7:0] Kp;
-  //(* mark_debug = "true" *) wire [7:0] Ki;
-  //(* mark_debug = "true" *) wire [6:0] Kd;
-  //(* mark_debug = "true" *) wire tuning_done;
-  //(* mark_debug = "true" *) wire [15:0] error;
-  //(* mark_debug = "true" *) wire [15:0] pid_output;
-  //(* mark_debug = "true" *) wire [15:0] pwm_duty_cycle;
-  //(* mark_debug = "true" *) wire [15:0] period_speed;
-  //(* mark_debug = "true" *) wire [15:0] period_reference_reg;
-  //(* mark_debug = "true" *) wire [3:0] tunerreset_autotune;
 endmodule
