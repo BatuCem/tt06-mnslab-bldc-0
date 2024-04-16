c2284659596530b9fdc17d1a77f95a05e36d1dca
BatuCem <63973576+BatuCem@users.noreply.github.com>
2024-04-17 00:00:51 +0300

    Update TOP_MODULE_1.v

--- src/TOP_MODULE_1.v
+++ src/TOP_MODULE_1.v
@@ -20,23 +20,18 @@
 
 module esc_1(
     input clk,
-    input rst,  // SW0
-    // I2C Ports
-    //inout sda,  // JB1
+    input rst,
     input sda_in,
     output sda_out,
     output sda_oe,
-    input scl,  // JB2
+    input scl, 
     
-    input pwm_en,   // SW15
-    input encoder_a,    // JB4
-    input encoder_b,    // JB7
-    output motor_positive,  // JB8
-    output motor_negative   // JB9
+    input pwm_en,   
+    input encoder_a,   
+    input encoder_b,   
+    output motor_positive,  
+    output motor_negative   
     
-    ////// TEST OUTPUTS
-    //output [6:0] sseg_out,
-    //output [3:0] ANen
     );
     
     // PID inputs
@@ -58,8 +53,6 @@ module esc_1(
     wire slow_clk2;
     wire [7:0] Kp_int_wire;
     wire [3:0] tunerreset_autotune;
-    
-    //wire [15:0] bldc_test_output; //TEST VARIABLE
      
     Divided_Clock divide_clk(
         . clk(clk),
@@ -71,19 +64,15 @@ module esc_1(
     I2C_SLAVE_1 I2C_SLAVE(
         . clk(slow_clk1),
         . rst(rst),
-        // I2C Ports
-        //. sda(sda),
         .sda_in(sda_in),
         .sda_out(sda_out),
         .sda_oe(sda_oe),
         . scl(scl),
-        // RAM control signals
         . write(write_i2c),
         . read_1(read_1_i2c),
         . index_1(index_1_i2c),
         . data_out(data_out_i2c),
         . data_in(data_out_ram),
-        // control signals
         . busy(busy_wire),
         . valid(valid_wire)
     );
@@ -97,14 +86,14 @@ module esc_1(
         . data_in(data_out_i2c),
         . data_out_1(data_out_ram),
         //
-        . pwm_period(pwm_period_wire),           // 0x40-0x41
-        . period_reference(period_reference_i2c_wire),  //0x40
-        . Kp_ext(Kp_ext_i2c_wire),            //0x41
-        . Ki_ext(Ki_ext_i2c_wire),            //0x42
-        . Kd_ext(Kd_ext_i2c_wire),            //0x43
+        . pwm_period(pwm_period_wire),          
+        . period_reference(period_reference_i2c_wire),  
+        . Kp_ext(Kp_ext_i2c_wire),            
+        . Ki_ext(Ki_ext_i2c_wire),            
+        . Kd_ext(Kd_ext_i2c_wire),         
         . Kp_int_i(Kp_int_wire), 
-        . override_internal_pid(override_internal_pid_i2c_wire),   //0x44
-        . tunerreset_autotune(tunerreset_autotune)             // 0x48
+        . override_internal_pid(override_internal_pid_i2c_wire),   
+        . tunerreset_autotune(tunerreset_autotune)             
     );
     
     bldc_esc_1 BLDC_ESC(
@@ -122,11 +111,8 @@ module esc_1(
       . Kd_ext(Kd_ext_i2c_wire),
       . override_internal_pid(override_internal_pid_i2c_wire),
       . Kp_int_o(Kp_int_wire),
-      //. outputvar(bldc_test_output),
       . motor_positive(motor_positive),
       . motor_negative(motor_negative)
     );
     
-    //////TEST INSTANTIATIONS
-    //hex4tosegments display16bits (clk,rst,pwm_en,bldc_test_output,sseg_out,ANen);
 endmodule
