49fbd052f7ea9b935665db9fcd44a5f9cf142861
BatuCem <63973576+BatuCem@users.noreply.github.com>
2024-04-16 23:56:04 +0300

    Update Divider32bit.v

--- src/Divider32bit.v
+++ src/Divider32bit.v
@@ -5,7 +5,6 @@
 // Create Date: 17.01.2024 11:44:23
 // Design Name: BLDC
 // Module Name: Divider32bit
-// Project Name: PID Controller for BLDC
 // Target Devices: 
 // Tool Versions: 
 // Description: 
@@ -21,20 +20,19 @@
 module Divider32bit (
    input      clk,
    input      reset,
-   input      start_division,  // Start signal for division
-   input [31:0] dividend,       // Dividend input
-   input [31:0] divisor,        // Divisor input
-   output reg [31:0] quotient,   // Quotient output
-   output reg [31:0] remainder,  // Remainder output
-   output reg division_active,   // Declaration of division_active
+   input      start_division,
+   input [31:0] dividend,    
+   input [31:0] divisor,  
+   output reg [31:0] quotient, 
+   output reg [31:0] remainder, 
+   output reg division_active,
   	output reg division_done
 );
 
   reg [5:0] division_cycle = 6'b100001;
-   // Internal registers
-   reg [32:0] store_divisor;      // Assigned as the original divisor
-   reg [32:0] shifting_divisor;     // One bit more than divisor
-   reg [31:0] shifting_dividend;     // Assigned as the original dividend
+   reg [32:0] store_divisor;   
+   reg [32:0] shifting_divisor;    
+   reg [31:0] shifting_dividend;    
 
    // State machine
    always @(posedge clk) begin
@@ -42,8 +40,8 @@ module Divider32bit (
          store_divisor <= 33'b0;
          shifting_divisor <= 33'b0;
          shifting_dividend <= dividend;
-         division_active <= 0;  // Initialize division_active
-      	 division_cycle <= 6'b100001; // Initialize division_cycle
+         division_active <= 0;
+      	 division_cycle <= 6'b100001; 
        	division_done<=0;
       end
 		else if (division_cycle == 6'b000000) begin
@@ -51,18 +49,14 @@ module Divider32bit (
          quotient <= shifting_dividend;
          remainder <= shifting_divisor[32:1];
          division_done<=1;
-         //remainder <= shifting_divisor[31:0];
-         //quotient <= shifting_divisor[31:0];
       end
       else if (start_division && (division_cycle > 0)) begin
-         // Move these assignments to the beginning of the block
          shifting_divisor <= {shifting_divisor[31:0], shifting_dividend[31]};
          shifting_dividend <= {shifting_dividend[30:0], 1'b0};
          
         if (shifting_divisor >= store_divisor) begin
-            store_divisor <= {1'b0, divisor};  // Assign divisor to store_divisor
+            store_divisor <= {1'b0, divisor};  
             shifting_divisor <= 33'b0;
-            //shifting_dividend <= dividend;
             shifting_divisor[32:1] <= shifting_divisor[31:0] - store_divisor[31:0];
             shifting_divisor[0] <= shifting_dividend[31]; 
             shifting_dividend[0] <= 1;
@@ -73,12 +67,6 @@ module Divider32bit (
       end else begin
          division_active <= 1;
       end
-
-      /* if (division_cycle == 1) begin
-         division_active <= 1;
-         //remainder <= shifting_dividend;
-         remainder <= shifting_divisor[32:1];
-      end */
      
    end
 endmodule
